b67f3f7278e0846216206044f802031bfa4ec082
lisazeyen <lisa.zeyen@web.de>
2021-04-19 14:18:15 +0200

    add home battery storage and inverter costs

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -820,7 +820,7 @@ def insert_electricity_distribution_grid(network):
                  e_cyclic=True,
                  e_nom_extendable=True,
                  carrier="home battery",
-                 capital_cost=costs.at['battery storage','fixed'],
+                 capital_cost=costs.at['home battery storage','fixed'],
                  lifetime=costs.at['battery storage','lifetime'])
 
     network.madd("Link",
@@ -829,7 +829,7 @@ def insert_electricity_distribution_grid(network):
                  bus1=nodes + " home battery",
                  carrier="home battery charger",
                  efficiency=costs.at['battery inverter','efficiency']**0.5,
-                 capital_cost=costs.at['battery inverter','fixed'],
+                 capital_cost=costs.at['home battery inverter','fixed'],
                  p_nom_extendable=True,
                  lifetime=costs.at['battery inverter','lifetime'])
 
