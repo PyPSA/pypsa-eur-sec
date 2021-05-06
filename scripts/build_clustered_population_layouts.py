2dfa9381b747f8890fb4d69e556d16ad8d7b3cf3
Fabian Neumann <fabian.neumann@outlook.de>
2021-05-06 09:22:29 +0200

    cl_pop_layout: blacken

--- scripts/build_clustered_population_layouts.py
+++ scripts/build_clustered_population_layouts.py
@@ -1,3 +1,4 @@
+"""Build clustered population layouts."""
 
 import geopandas as gpd
 import xarray as xr
@@ -6,27 +7,29 @@ import atlite
 import helper
 
 
-cutout = atlite.Cutout(snakemake.config['atlite']['cutout_name'],
-                       cutout_dir=snakemake.config['atlite']['cutout_dir'])
+if __name__ == '__main__':
+
+    cutout = atlite.Cutout(snakemake.config['atlite']['cutout'])
+
+    clustered_busregions_as_geopd = gpd.read_file(
+        snakemake.input.regions_onshore).set_index('name', drop=True)
+
+    clustered_busregions = pd.Series(
+        clustered_busregions_as_geopd.geometry, index=clustered_busregions_as_geopd.index)
+
+    helper.clean_invalid_geometries(clustered_busregions)
+
+    I = cutout.indicatormatrix(clustered_busregions)
 
 
-clustered_busregions_as_geopd = gpd.read_file(snakemake.input.regions_onshore).set_index('name', drop=True)
+    items = ["total", "urban", "rural"]
 
-clustered_busregions = pd.Series(clustered_busregions_as_geopd.geometry, index=clustered_busregions_as_geopd.index)
-
-helper.clean_invalid_geometries(clustered_busregions)
-
-I = cutout.indicatormatrix(clustered_busregions)
+    pop = pd.DataFrame(columns=items,
+                    index=clustered_busregions.index)
 
 
-items = ["total","urban","rural"]
+    for item in items:
+        pop_layout = xr.open_dataarray(snakemake.input['pop_layout_'+item])
+        pop[item] = I.dot(pop_layout.stack(spatial=('y', 'x')))
 
-pop = pd.DataFrame(columns=items,
-                   index=clustered_busregions.index)
-
-
-for item in items:
-    pop_layout = xr.open_dataarray(snakemake.input['pop_layout_'+item])
-    pop[item] = I.dot(pop_layout.stack(spatial=('y', 'x')))
-
-pop.to_csv(snakemake.output.clustered_pop_layout)
+    pop.to_csv(snakemake.output.clustered_pop_layout)
