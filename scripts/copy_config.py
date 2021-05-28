f33a20410fe7ef56569a6dd427c09658678debaf
Fabian Neumann <fabian.neumann@outlook.de>
2021-05-28 11:48:13 +0200

    plot/summary: cosmetic changes including: - matplotlibrc for default style and backend - remove unused config options - option to configure geomap colors - option to configure geomap bounds

--- scripts/copy_config.py
+++ scripts/copy_config.py
@@ -1,10 +1,14 @@
 
 from shutil import copy
 
-files = ["config.yaml",
-         "Snakefile",
-         "scripts/solve_network.py",
-         "scripts/prepare_sector_network.py"]
+files = [
+    "config.yaml",
+    "Snakefile",
+    "scripts/solve_network.py",
+    "scripts/prepare_sector_network.py"
+]
 
-for f in files:
-    copy(f,snakemake.config['summary_dir'] + '/' + snakemake.config['run'] + '/configs/')
+if __name__ == '__main__':
+
+    for f in files:
+        copy(f,snakemake.config['summary_dir'] + '/' + snakemake.config['run'] + '/configs/')
