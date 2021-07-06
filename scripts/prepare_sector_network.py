3e07d46a5468decb68f67f6b252620dc88d6c657
Fabian Neumann <fabian.neumann@outlook.de>
2021-07-06 14:12:22 +0200

    switch to new H2 (g) pipeline cost

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -941,9 +941,9 @@ def add_storage(n, costs):
         p_min_pu=-1,
         p_nom_extendable=True,
         length=h2_links.length.values,
-        capital_cost=costs.at['H2 pipeline', 'fixed'] * h2_links.length.values,
+        capital_cost=costs.at['H2 (g) pipeline', 'fixed'] * h2_links.length.values,
         carrier="H2 pipeline",
-        lifetime=costs.at['H2 pipeline', 'lifetime']
+        lifetime=costs.at['H2 (g) pipeline', 'lifetime']
     )
 
     n.add("Carrier", "battery")
