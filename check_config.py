6a5a0c645cac87f97cc90f3613dc3d285f3701ac
energyls <leon1.schumm@oth-regensburg.de>
2022-09-01 14:16:08 +0200

    chore: rename new to old config

--- check_config.py
+++ check_config.py
@@ -3,10 +3,10 @@ from deepdiff import DeepDiff
 
 if __name__ == '__main__':
 
-    with open("config.default.yaml", 'r') as fp:
+    with open("config.default.old.yaml", 'r') as fp:
         config_old = yaml.safe_load(fp)
 
-    with open("config.default.new.yaml", 'r') as fp:
+    with open("config.default.yaml", 'r') as fp:
         config_new = yaml.safe_load(fp)
 
     ddiff = DeepDiff(config_old, config_new, ignore_order=True)
