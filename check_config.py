a3e3af8e941151f8aeca127e3504dd572c7f9b94
energyls <leon1.schumm@oth-regensburg.de>
2022-09-01 14:06:47 +0200

    feat: implement diff check

--- check_config.py
+++ check_config.py
@@ -1,2 +1,13 @@
-import pandas as pd
+import yaml
+from deepdiff import DeepDiff
 
+if __name__ == '__main__':
+
+    with open("config.default.yaml", 'r') as fp:
+        config_old = yaml.safe_load(fp)
+
+    with open("config.default.new.yaml", 'r') as fp:
+        config_new = yaml.safe_load(fp)
+
+    ddiff = DeepDiff(config_old, config_new, ignore_order=True)
+    print(ddiff)
